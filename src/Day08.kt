b858164e9a8e6bebce307a8d53fe80b8eb7f971f
Lucas Ancelmo Dias <lucas.ancelmo547@gmail.com>
2021-12-08 20:04:43 -0300

    Refactoring

--- src/Day08.kt
+++ src/Day08.kt
@@ -1,38 +1,41 @@
+import kotlin.system.measureTimeMillis
+import kotlin.time.Duration.Companion.milliseconds
+
 fun main(){
     val numbers = readInput("inputday8")
+    measureTimeMillis { solve(numbers) }.also { println("Time to compute ${it.milliseconds}") }
+}
 
+fun solve(numbers: List<String>) {
     val lengths = arrayListOf(2, 4, 3, 7)
 
     var part1 = 0
     var part2 = 0
-    for(num in numbers){
+    for (num in numbers) {
         val sp = num.split(" | ")
         val left = sp[0].split(" ")
         val right = sp[1].split(" ")
 
-
         val occurrences = mutableMapOf<String, Int>()
-        for(signal in left){
-            for(char in signal){
+        for (signal in left) {
+            for (char in signal) {
                 occurrences.merge(char.toString(), 1, Integer::sum)
             }
         }
         var m = 1000
-        for(input in right){
-
-            if(lengths.contains(input.length)){
+        for (input in right) {
+            if (lengths.contains(input.length)) {
                 part1 += 1
             }
-
             var inputSum = 0
 
-            for(letter in input) inputSum += occurrences[letter.toString()]!!
+            for (letter in input) inputSum += occurrences[letter.toString()]!!
             /*
             * For each letter, count how many times it appears in the list. For example, "a" appears 8 times, "b" appears 6 times, "c" appears 8 times etc.
             * When you see something like "cf" or "acf", add up those counts. For example, "cf" becomes 8 + 9 = 17, because "c" appears 8 times and "f" appears 9 times.
             * Now 17 is the unique id for the 1 digit, cf.
             * */
-            val value = when(inputSum){
+            val value = when (inputSum) {
                 42 -> 0
                 17 -> 1
                 34 -> 2
@@ -52,7 +55,4 @@ fun main(){
     }
     println("Part 1: $part1")
     println("Part 2: $part2")
-
-
-
 }
